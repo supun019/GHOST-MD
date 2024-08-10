7d6f8536a775a0e7a08fa4bcf4423b0a317a06e4
GHOST-MD <minethgayansa@gmail.com>
2024-08-10 21:32:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,84 @@
-const fs = require('fs');
-if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+//GHOST-MD code
+const fs = require("fs-extra");
+if (fs.existsSync(".env"))
+  require("dotenv").config({ path: __dirname + "/.env" });
 
-function convertToBool(text, fault = 'true') {
-    return text === fault ? true : false;
-}
+//=======[dependencies]====================//
+global.SESSION_ID = process.env.SESSION_ID || "";
+global.MONGODB = process.env.MONGODB_URI || "";
+global.DATABASE_URL = process.env.DATABASE_URL || "";
+global.sudo = process.env.SUDO
+  ? process.env.SUDO.replace(/[\s+]/g, "")
+  : "null";
+global.owner = process.env.OWNER_NUMBER
+  ? process.env.OWNER_NUMBER.replace(/[\s+]/g, "")
+  : "94741140620";
+global.THUMB_IMAGE =
+  process.env.THUMB_IMAGE ||
+  process.env.IMAGE ||
+  "https://telegra.ph/file/26fa981907598d22aa4e1.jpg,https://telegra.ph/file/a189d4040c4bd9150b7b4.jpg";
+global.userImages =
+  process.env.USER_IMAGES ||
+  "https://telegra.ph/file/26fa981907598d22aa4e1.jpg";
+///===========[global iMPORTS]====================//
+
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+  menu: process.env.MENU || "",
+  HANDLERS: process.env.PREFIX || ".",
+  BRANCH: process.env.BRANCH || "main",
+  VERSION: process.env.VERSION || "1.0.0",
+  caption: process.env.CAPTION || "`GHOST-MD`",
+  author: process.env.PACK_AUTHER || "GHOST-MD",
+  packname: process.env.PACK_NAME || "GHOST-MD",
+  botname: process.env.BOT_NAME || "GHOST-MD",
+  ownername: process.env.OWNER_NAME || "GHOST-MD",
+  errorChat: process.env.ERROR_CHAT || "",
+  KOYEB_API: process.env.KOYEB_API || "false",
+  REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
+  OPENAI_API_KEY: process.env.OPENAI_API_KEY || "",
+  HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
+  HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || "",
+  antilink_values: process.env.ANTILINK_VALUES || "all",
+  HEROKU: process.env.HEROKU_APP_NAME && process.env.HEROKU_API_KEY,
+  aitts_Voice_Id: process.env.AITTS_ID || "37",
+  ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "",
+  WORKTYPE: process.env.WORKTYPE || process.env.MODE || "public",
+  LANG: (process.env.THEME || "WhatsApp").toUpperCase(),
 };
+global.port = process.env.PORT;
+global.appUrl = process.env.APP_URL || "";
+global.email = "";
+global.location = "";
+global.allowJids = process.env.ALLOW_JID || "null";
+global.blockJids = process.env.BLOCK_JID || "null";
+global.timezone = process.env.TZ || process.env.TIME_ZONE || "Africa/Lagos";
+global.github = process.env.GITHUB || "https://github.com/Cyberghost352/GHOST-MD.git";
+global.gurl = process.env.GURL || "https://chat.whatsapp.com/DhCdan60qzH0yoIxmybU3i";
+global.website = process.env.GURL || "https://chat.whatsapp.com/DhCdan60qzH0yoIxmybU3i";
+global.devs = "94741140620,94718919262,94787438929";
+global.msg_style = process.env.STYLE || "4";
+global.session_reset = process.env.SS_RESET || "false";
+global.gdbye = process.env.GOODBYE || "false";
+global.wlcm = process.env.WELCOME || "false";
+global.warncount = process.env.WARN_COUNT || 3;
+global.disablepm = process.env.DISABLE_PM || "false";
+(global.disablegroup = process.env.DISABLE_GROUPS || "false"),
+  (global.MsgsInLog = process.env.MSGS_IN_LOG || "true");
+global.waPresence = process.env.WAPRESENCE || "recording";
+global.readcmds = process.env.READ_COMMAND || "false";
+global.readmessage = process.env.READ_MESSAGE || "false";
+global.readmessagefrom = process.env.READ_MESSAGE_FROM || "null";
+global.read_status = process.env.AUTO_READ_STATUS || "ture";
+global.save_status = process.env.AUTO_SAVE_STATUS || "false";
+global.save_status_from = process.env.SAVE_STATUS_FROM || "null";
+global.read_status_from = process.env.READ_STATUS_FROM || "null";
+global.api_smd = "";
+global.scan = "";
+global.isMongodb = false;
+let file = require.resolve(__filename);
+fs.watchFile(file, () => {
+  fs.unwatchFile(file);
+  console.log(`Update'${__filename}'`);
+  delete require.cache[file];
+  require(file);
+});
