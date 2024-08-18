fdc4e9963e131173ab2cf3e0f6716e60cd521233
GHOST-MD <minethgayansa@gmail.com>
2024-08-18 13:52:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,4 +8,5 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/397000a07a1deb7fad9c2.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "HELLO,IM GHOST-MD I AM ALIVE NOW!",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
