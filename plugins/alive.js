4bdba3606e5ae701f670aad113295f59060efd3f
GHOST-MD <minethgayansa@gmail.com>
2024-09-01 18:19:31 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -12,7 +12,7 @@ const alive = async (m, Matrix) => {
   const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).toLowerCase() : '';
     if (['alive', 'uptime', 'runtime'].includes(cmd)) {
 
-  const uptimeMessage = `*HANSAMAL-MD Status Overview*
+  const uptimeMessage = `*GHOST-MD Status Overview*
 _________________________________________
 
 *📆 ${days} Day*
