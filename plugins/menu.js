9ce9a073e3082acb5132efd6be0619e137bd96e3
GHOST-MD <minethgayansa@gmail.com>
2024-09-01 10:23:11 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -61,7 +61,7 @@ let dec = `
 
  ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɢʜᴏꜱᴛ-ᴍᴅ
 `
-await conn.sendMessage(from,{image:{url: `https://telegra.ph/file/77a5adb1b4462dd384223.jpg`},caption:dec},{quoted:mek});
+await conn.sendMessage(from,{image:{url: `https://telegra.ph/file/397000a07a1deb7fad9c2.jpg`},caption:dec},{quoted:mek});
 
 }catch(e){
 console.log(e)
