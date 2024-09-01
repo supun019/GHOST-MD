3c4322bebf50e6e067a10648e5732bc5bb30142f
NETHA Tech <150256662+Nethmikakaushalyaherath@users.noreply.github.com>
2024-09-01 09:57:53 +0530

    Update ai.js

--- plugins/ai.js
+++ plugins/ai.js
@@ -1,16 +1,26 @@
 const config = require('../config')
 const {cmd , commands} = require('../command')
-const { fetchJson } = require('../lib/functions')
+
 cmd({
-    pattern: "ai",
-    desc: "ai chat",
+    pattern: "alive",
+    desc: "Check bot online or no.",
     category: "main",
+    react: "👋",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-let data = await fetchjson(`https://chatgptforprabath-md.vercel.app/api/gptv1?q=${q}`)
-return reply(`${data.data}`)
+
+let des = `👋 𝙷𝚎𝚕𝚕𝚘 𝙸'𝚖 𝚊𝚕𝚒𝚟𝚎 𝚗𝚘𝚠
+
+*Im NETHU-MD Whatsapp Bot Create By Nethu Max* 
+
+I am nethu md whatsapp bot. How can I help you.
+To get the menu, type as menu. If you need to know something about the bot,
+type as owner and direct the question to me. Good day.
+
+`
+return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: des},{quoted: mek})
 }catch(e){
 console.log(e)
 reply(`${e}`)
