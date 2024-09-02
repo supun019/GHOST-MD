58ec27609f82963596a9e76c09d328b23d00c681
NETHA Tech <150256662+Nethmikakaushalyaherath@users.noreply.github.com>
2024-09-02 16:05:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/397000a07a1deb7fad9c2.jpg",
 MONGODB: process.env.MONGODB || "",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
