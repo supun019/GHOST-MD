15c3b98713de7c6e7cd0c608e6294867e8a0a7e1
GHOST-MD <minethgayansa@gmail.com>
2024-09-02 18:28:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,5 +9,4 @@ SESSION_ID: process.env.SESSION_ID || "",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/397000a07a1deb7fad9c2.jpg",
 MONGODB: process.env.MONGODB || "",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-global.MsgsInLog = process.env.MSGS_IN_LOG || "true"
 };
