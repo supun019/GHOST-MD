5c542142512fa1757e66b2ce6d01599735de3a3f
GHOST-MD <minethgayansa@gmail.com>
2024-09-02 19:30:40 +0530

    Update song+video.js

--- plugins/song+video.js
+++ plugins/song+video.js
@@ -7,6 +7,7 @@ cmd({
     pattern: "song",
     desc: "download songs",
     category: "download ",
+    react: "💿",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
@@ -53,6 +54,7 @@ cmd({
     pattern: "video",
     desc: "download videos",
     category: "download ",
+    react: "🎥",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
