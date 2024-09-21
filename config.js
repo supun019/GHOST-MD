764ccbe9d240002144cb26b86e4b135719b6fe7f
Dagaya <lakshansupun877@gmail.com>
2024-09-21 13:40:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "jfolBQBS#E_ny55irG1id1LXJ-197ymEcXcVEHKXwd9hDaNqcjIE",
+SESSION_ID: process.env.SESSION_ID || "Ru9jUSgJ#a7DdhG1zYemVENX0s3cR2IbZ6WzUZobw767M1hisn6k",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/397000a07a1deb7fad9c2.jpg",
 MONGODB: process.env.MONGODB || "",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
